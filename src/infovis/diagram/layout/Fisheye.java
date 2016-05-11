d111e177acff6dd55f6503fb0222635a91a0a130
jaboard <jannis-bossert@web.de>
2016-05-11 17:44:20 +0200

    minor_preparations_for_fisheye

--- src/infovis/diagram/layout/Fisheye.java
+++ src/infovis/diagram/layout/Fisheye.java
@@ -4,6 +4,7 @@ import infovis.debug.Debug;
 import infovis.diagram.Model;
 import infovis.diagram.View;
 import infovis.diagram.elements.Edge;
+import infovis.diagram.elements.Element;
 import infovis.diagram.elements.Vertex;
 
 import java.util.Iterator;
@@ -19,8 +20,33 @@ public class Fisheye implements Layout{
 	}
 
 	public Model transform(Model model, View view) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+//		for (Element element: model.getElements()){
+//			element.setHeight(3);
+//		}
 		return null;
+	}
+	
+	
+	static void fish(int x, int y, double d){
+		
+		double pNormX = 1;
+		double pFocusX = 1;
+		double pBoundaryX = 1;
+		double dMaxX = 1;
+		
+		if (pNormX > pFocusX) {
+			dMaxX = pBoundaryX - pFocusX;
+		} else if ( pNormX < pFocusX) {
+			dMaxX = - pFocusX;
+		}
+		double dNormX = pNormX - pFocusX;
+		double valueX = dNormX / dMaxX;
+		double gX = ((d + 1) * valueX) / (d * valueX + 1);
+		double pFishX = pFocusX + gX * dMaxX;
 	}
 	
 }
